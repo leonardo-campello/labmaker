1b37d5e649f4ee739d421da2a6ed566bc6b57bc5
Leonardo Campello <leo.campello@hotmail.com>
2022-02-23 10:00:28 -0300

    Template Menu e Topbar

---
+++ templates/menu.php
@@ -0,0 +1,77 @@
+<!-- Header start -->
+<header id="header" class="header-two">
+  <div class="site-navigation">
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+              <nav class="navbar navbar-expand-lg navbar-light p-0">
+                
+                <div class="logo">
+                    <a class="d-block" href="index-2.html">
+                      <img loading="lazy" src="images/logo-labmaker.webp" alt="Logo do LabMaker">
+                    </a>
+                </div><!-- logo end -->
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div id="navbar-collapse" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav ml-auto align-items-center">
+                      <li class="nav-item dropdown active">
+                          <a href="/" class="nav-link dropdown-toggle" data-toggle="dropdown">Home</a>                          
+                      </li>
+
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Nossa história</a>                          
+                      </li>
+
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Equipe</a>                          
+                      </li>
+              
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Academia Maker<i class="fa fa-angle-down"></i></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="projects.html">Artesanato Digital</a></li>
+                            <li><a href="projects-single.html">Programação com Arduino</a></li>
+                            <li><a href="projects-single.html">Cidades para brincar</a></li>
+                            <li><a href="projects-single.html">Entrevistas Maker</a></li>
+                            <li><a href="projects-single.html">Sertão Maker</a></li>
+                            <li><a href="projects-single.html">Sertão Maker: Empreendedorismo e Inovação</a></li>
+                          </ul>
+                      </li>
+              
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Comunidade Maker<i class="fa fa-angle-down"></i></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="services.html">Equipamentos IF Salgueiro</a></li>
+                            <li><a href="service-single.html">Equipamentos Labs Makers</a></li>
+                          </ul>
+                      </li>
+
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Agenda</a>                          
+                      </li>
+              
+                      <li class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Contato</a>                          
+                      </li>
+              
+                      <li class="header-get-a-quote">
+                          <a class="btn btn-primary" href="contact.html">Login</a>
+                      </li>
+                    </ul>
+                </div>
+              </nav>
+          </div>
+          <!--/ Col end -->
+        </div>
+        <!--/ Row end -->
+    </div>
+    <!--/ Container end -->
+
+  </div>
+  <!--/ Navigation end -->
+</header>
+<!--/ Header end -->
