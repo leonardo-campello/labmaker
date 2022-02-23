5df93126f7fbcb446687fa091e8604d2d38132dd
Leonardo Campello <leo.campello@hotmail.com>
2022-02-23 09:46:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -53,18 +53,18 @@
               <div class="col-lg-4 col-md-4 top-social text-center text-md-right">
                 <ul class="list-unstyled">
                     <li>
-                      <a title="Facebook" href="https://facebbok.com/themefisher.com">
+                      <!-- <a title="Facebook" href="https://facebbok.com/themefisher.com">
                           <span class="social-icon"><i class="fab fa-facebook-f"></i></span>
-                      </a>
-                      <a title="Twitter" href="https://twitter.com/themefisher.com">
+                      </a> -->
+                      <!-- <a title="Twitter" href="https://twitter.com/themefisher.com">
                           <span class="social-icon"><i class="fab fa-twitter"></i></span>
-                      </a>
-                      <a title="Instagram" href="https://instagram.com/themefisher.com">
+                      </a> -->
+                      <!-- <a title="Instagram" href="https://instagram.com/themefisher.com">
                           <span class="social-icon"><i class="fab fa-instagram"></i></span>
-                      </a>
-                      <a title="Linkdin" href="https://github.com/themefisher.com">
+                      </a> -->
+                      <!-- <a title="Linkdin" href="https://github.com/themefisher.com">
                           <span class="social-icon"><i class="fab fa-github"></i></span>
-                      </a>
+                      </a> -->
                       <a title="YouTube" href="https://www.youtube.com/channel/UCME7pYi-_Du-R8s3RYfA_mw">
                           <span class="social-icon"><i class="fab fa-youtube"></i></span>
                       </a>
